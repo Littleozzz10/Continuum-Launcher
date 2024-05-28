b2654811d7531b33e8646ebe3020dd068b16ed3b
Littleozzz10 <41541673+Littleozzz10@users.noreply.github.com>
2024-05-28 13:32:28 -0700

    Removed Sandcastle documentation

--- Sandcastle/Code Examples/OzzzFramework/Helper/ozzz_help_arraystring.cs
+++
@@ -1,8 +0,0 @@
-using Helper = OzzzFramework.Helper;
-
-string[] fruit = { "Apple", "Banana", "Orange" }
-
-string stringOfArray = Helper.ArrayString(fruit, "... ");
-
-// Expected Output:
-// "Apple... Banana... Orange"
